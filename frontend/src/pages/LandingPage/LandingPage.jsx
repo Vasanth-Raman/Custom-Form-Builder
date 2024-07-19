257de04547010fbd9ed19bbddd84de5a8af5d342
VasaR <vasanthias52@gmail.com>
2024-07-20 04:26:24 +0530

    OldvsNew section completed in landing page

--- frontend/src/pages/LandingPage/LandingPage.jsx
+++ frontend/src/pages/LandingPage/LandingPage.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import styles from "./LandingPage.module.css";
 import Header from "../../components/LandingComponents/Header/Header";
 import HeroSection from "../../components/LandingComponents/HeroSection/HeroSection";
+import OldvsNew from "../../components/LandingComponents/OldvsNew/OldvsNew";
 
 const LandingPage = () => {
   return (
     <div>
       <Header />
       <HeroSection />
+      <OldvsNew />
     </div>
   );
 };
