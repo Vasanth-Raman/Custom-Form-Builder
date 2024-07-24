8cf934dcbe2f8d1f84dd06a73b0e4200d4827a4a
VasaR <vasanthias52@gmail.com>
2024-07-24 13:09:14 +0530

    server updated with routes

--- backend/server.js
+++ backend/server.js
@@ -4,23 +4,18 @@ const connectDb = require("./config/dbConnect");
 const verifyToken = require("./middleware/verifyToken");
 const userRouter = require("./routes/userRouter");
 const folderRouter = require("./routes/folderRouter");
+const formRouter = require("./routes/formRouter");
 
 const PORT = process.env.PORT || 3000;
-
 const app = express();
 
 app.use(express.json());
 
-app.get("/health", (req, res) => {
-  res.json({
-    success: true,
-    message: "Good to go",
-  });
-});
-
 app.use("/api/v1/user", userRouter);
 
-app.use("/api/v1/folders", verifyToken, folderRouter);
+app.use("/api/v1/folder", verifyToken, folderRouter);
+
+app.use("/api/v1/form", verifyToken, formRouter);
 
 app.listen(PORT, async () => {
   await connectDb();
