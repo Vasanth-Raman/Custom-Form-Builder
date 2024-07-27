3f4dc93acdc292367dbc92a97500d2dcb394157c
VasaR <vasanthias52@gmail.com>
2024-07-28 02:12:23 +0530

    validations done for login and register page

--- frontend/src/components/AuthComponents/LogIn/LogIn.jsx
+++ frontend/src/components/AuthComponents/LogIn/LogIn.jsx
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Login.module.css";
 import DoubleTriange from "../../../assets/icons/double-triangle.svg";
 import SandalEllipse from "../../../assets/icons/ellipse-sandal.svg";
 import PinkEllipse from "../../../assets/icons/ellipse-pink.svg";
 import BackArrow from "../../../assets/icons/arrow-back.svg";
-import { Navigate, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import validateLogin from "../../../validations/validateLogin";
 
 const LogIn = () => {
+  const initialValues = { email: "", password: "" };
+  const [credentials, setCredentials] = useState(initialValues);
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
+
   const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setCredentials((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = () => {
+    setFormErrors(validateLogin(credentials));
+    setIsSubmit(true);
+  };
+
+  useEffect(() => {
+    if (Object.keys(formErrors).length === 0 && isSubmit) {
+      // Logic goes here
+    }
+  }, [formErrors, isSubmit]);
+
   return (
     <div className={styles.signUpContainer}>
       <img
@@ -27,39 +50,68 @@ const LogIn = () => {
         alt="SandalEllipse"
       />
       <img className={styles.pinkEllipse} src={PinkEllipse} alt="PinkEllipse" />
-      <div className={styles.credContainer}>
+
+      <form
+        className={styles.credContainer}
+        onSubmit={(e) => e.preventDefault()}
+        noValidate
+      >
         <div className={styles.fieldsContainer}>
           <div className={styles.fields}>
-            <label htmlFor="email">Email</label>
+            <label
+              htmlFor="email"
+              className={`${
+                formErrors.email ? styles.errorText : styles.label
+              }`}
+            >
+              Email
+            </label>
             <input
               type="email"
               id="email"
-              required
+              name="email"
               autoComplete="on"
               placeholder="Enter your email"
+              value={credentials.email}
+              onChange={handleChange}
+              className={`${
+                formErrors.email ? styles.errorBorder : styles.input
+              }`}
             />
+            <small>{formErrors.email}</small>
           </div>
 
           <div className={styles.fields}>
-            <label htmlFor="password">Password</label>
+            <label
+              htmlFor="password"
+              className={`${
+                formErrors.password ? styles.errorText : styles.label
+              }`}
+            >
+              Password
+            </label>
             <input
               type="password"
               id="password"
-              required
-              autoComplete="on"
-              placeholder="Enter your password"
+              name="password"
+              placeholder="**********"
+              value={credentials.password}
+              onChange={handleChange}
+              className={`${
+                formErrors.password ? styles.errorBorder : styles.input
+              }`}
             />
-            <small>enter same password in both the fields</small>
+            <small>{formErrors.password}</small>
           </div>
         </div>
         <div className={styles.btnContainer}>
-          <button>Log In</button>
+          <button onClick={handleSubmit}>Log In</button>
           <p>
             Don't have an account ?{" "}
             <span onClick={() => navigate("/auth/register")}>Register now</span>
           </p>
         </div>
-      </div>
+      </form>
     </div>
   );
 };
--- frontend/src/components/AuthComponents/SignUp/SignUp.jsx
+++ frontend/src/components/AuthComponents/SignUp/SignUp.jsx
@@ -1,13 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./SignUp.module.css";
 import DoubleTriange from "../../../assets/icons/double-triangle.svg";
 import SandalEllipse from "../../../assets/icons/ellipse-sandal.svg";
 import PinkEllipse from "../../../assets/icons/ellipse-pink.svg";
 import BackArrow from "../../../assets/icons/arrow-back.svg";
 import { useNavigate } from "react-router-dom";
+import validateRegister from "../../../validations/validateRegister";
 
 const SignUp = () => {
+  const initialValues = {
+    userName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  };
+  const [credentials, setCredentials] = useState(initialValues);
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
   const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setCredentials((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = () => {
+    setFormErrors(validateRegister(credentials));
+    setIsSubmit(true);
+  };
+
+  useEffect(() => {
+    if (Object.keys(formErrors).length === 0 && isSubmit) {
+      // Logic goes here
+    }
+  }, [formErrors, isSubmit]);
+
   return (
     <div className={styles.signUpContainer}>
       <img
@@ -27,58 +54,113 @@ const SignUp = () => {
         alt="SandalEllipse"
       />
       <img className={styles.pinkEllipse} src={PinkEllipse} alt="PinkEllipse" />
-      <div className={styles.credContainer}>
+      <form
+        className={styles.credContainer}
+        onSubmit={(e) => e.preventDefault()}
+        noValidate
+      >
         <div className={styles.fieldsContainer}>
           <div className={styles.fields}>
-            <label htmlFor="username">Username</label>
+            <label
+              htmlFor="username"
+              className={`${
+                formErrors.userName ? styles.errorText : styles.label
+              }`}
+            >
+              Username
+            </label>
             <input
               type="text"
               id="username"
-              required
+              name="userName"
+              value={credentials.userName}
+              onChange={handleChange}
               autoComplete="on"
               placeholder="Enter a username"
+              className={`${
+                formErrors.userName ? styles.errorBorder : styles.input
+              }`}
             />
+            <small>{formErrors.userName}</small>
           </div>
           <div className={styles.fields}>
-            <label htmlFor="email">Email</label>
+            <label
+              htmlFor="email"
+              className={`${
+                formErrors.email ? styles.errorText : styles.label
+              }`}
+            >
+              Email
+            </label>
             <input
               type="email"
               id="email"
-              required
+              name="email"
+              value={credentials.email}
+              onChange={handleChange}
               autoComplete="on"
               placeholder="Enter your email"
+              className={`${
+                formErrors.email ? styles.errorBorder : styles.input
+              }`}
             />
+            <small>{formErrors.email}</small>
           </div>
           <div className={styles.fields}>
-            <label htmlFor="password">Password</label>
+            <label
+              htmlFor="password"
+              className={`${
+                formErrors.password ? styles.errorText : styles.label
+              }`}
+            >
+              Password
+            </label>
             <input
               type="password"
               id="password"
-              required
+              name="password"
+              value={credentials.password}
+              onChange={handleChange}
               autoComplete="on"
-              placeholder="Enter a password"
+              placeholder="**********"
+              className={`${
+                formErrors.password ? styles.errorBorder : styles.input
+              }`}
             />
+            <small>{formErrors.password}</small>
           </div>
           <div className={styles.fields}>
-            <label htmlFor="confirm-password">Confirm Password</label>
+            <label
+              htmlFor="confirmPassword"
+              className={`${
+                formErrors.confirmPassword ? styles.errorText : styles.label
+              }`}
+            >
+              Confirm Password
+            </label>
             <input
               type="password"
-              id="confirm-password"
-              required
+              id="confirmPassword"
+              name="confirmPassword"
+              value={credentials.confirmPassword}
+              onChange={handleChange}
               autoComplete="on"
-              placeholder="Confirm your password"
+              placeholder="**********"
+              className={`${
+                formErrors.confirmPassword ? styles.errorBorder : styles.input
+              }`}
             />
-            <small>enter same password in both the fields</small>
+            <small>{formErrors.confirmPassword}</small>
           </div>
         </div>
         <div className={styles.btnContainer}>
-          <button>Sign Up</button>
+          <button onClick={handleSubmit}>Sign Up</button>
           <p>
             Already have an account ?{" "}
             <span onClick={() => navigate("/auth/login")}>Login</span>
           </p>
         </div>
-      </div>
+      </form>
     </div>
   );
 };
